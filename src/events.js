4d67783dedb77fd60fa4645929ea73781249a28c
cpc-newHaven <138483630+cpc-newHaven@users.noreply.github.com>
2023-07-03 07:45:06 -0400

    uploaded basic cpc structure

---
+++ src/events.js
@@ -0,0 +1,28 @@
+new Vue({
+    el: '#app',
+    data: {
+        events: [
+            {
+                id: 1,
+                name: 'Event 1',
+                description: 'Sample event description 1',
+                date: '2023-07-01',
+                image: 'https://example.com/image1.jpg'
+            },
+            {
+                id: 2,
+                name: 'Event 2',
+                description: 'Sample event description 2',
+                date: '2023-07-02',
+                image: 'https://example.com/image2.jpg'
+            },
+            {
+                id: 3,
+                name: 'Event 3',
+                description: 'Sample event description 3',
+                date: '2023-07-03',
+                image: 'https://example.com/image3.jpg'
+            }
+        ]
+    }
+});
